ed488b4fea665b3a574dbe49a5623a414a841c5e
theordore schrock <theorschrock@gmail.com>
2020-11-30 16:14:46 -0700

    added option for running a test on a development version of a package

--- R/runTests.R
+++ R/runTests.R
@@ -1,11 +1,12 @@
 #' Run all tests in the testthat dir of a package.
 
 #' @name runTests
-#' @param name [string] the test name
-#' @param package  [string]  Defaults to current_pkg()
+#' @param name \code{[string]} the test name
+#' @param package  \code{[string]}  Defaults to current_pkg()
+#' @param dev_version \code{[logical(1)]} If true, the test will be run using \code{devtools::load_all()} rather than \code{library(PKG_NAME)}.
 #' @return \code{runTests}: [Print(TestResults)]
 #' @export
-runTests <- function(name=NULL,package = current_pkg()) {
+runTests <- function(name=NULL,package = current_pkg(),dev_version=FALSE) {
     # Run all tests in the testthat dir of a package
     assert_string(package)
      if(nnull(name)){
@@ -14,12 +15,25 @@ runTests <- function(name=NULL,package = current_pkg()) {
        assert_file_exists(file)
        pathout =glue("~/{package}/tests/testthat/{name}.R")
        testDeps<-expr_glue(library({get_project_testing_deps()}))
-       out= run_isolated({
-         library(testthat)
-         !!!testDeps
-         library(!!package,character.only = TRUE)
-         testthat::test_file(path=!! pathout)
-       })
+       if(dev_version){
+         out= run_isolated({
+           library(testthat)
+           !!!testDeps
+           devtools::load_all(quiet=TRUE)
+           testthat::test_file(path=!! pathout)
+         })
+
+       }else{
+
+         out= run_isolated({
+           library(testthat)
+           !!!testDeps
+           library(!!package,character.only = TRUE)
+           testthat::test_file(path=!! pathout)
+         })
+
+       }
+
 
      }else{
      out= run_isolated({
