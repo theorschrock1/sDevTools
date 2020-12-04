c84420886036c6aac7d0e81b3e52d0c0b990aad2
theordore schrock <theorschrock@gmail.com>
2020-12-04 14:54:18 -0700

    added tests for "rox_comments"

---
+++ tests/testthat/_snaps/rox_comments.md
@@ -0,0 +1,9 @@
+# rox_comments
+
+    Code
+      rox_comments(
+        "these are roxygen comments\n  more comments\n  more comments\n  @noRd")
+    Output
+      [1] "#' these are roxygen comments" "#' more comments"             
+      [3] "#' more comments"              "#' @noRd"                     
+
--- tests/testthat/test_rox_comments.R
+++ tests/testthat/test_rox_comments.R
@@ -1,11 +1,6 @@
-library(sDevTools)
-context("rox_comments")
-test_that("rox_comments(\"these are roxygen comments\\n  more comments\\n  more comments\\n  @noRd\")", 
-    {
-        output <- {
-            rox_comments("these are roxygen comments\n  more comments\n  more comments\n  @noRd")
-        }
-        expect_equal(test_summary(output), list(N = 4L, class = "character", 
-            values = c("#' these are roxygen comments", "#' more comments", 
-            "#' @noRd")))
-    })
+test_that("rox_comments", {
+  local_edition(3)
+  expect_snapshot(rox_comments(
+    "these are roxygen comments\n  more comments\n  more comments\n  @noRd"),
+  cran = TRUE)
+})
