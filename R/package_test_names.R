82b0bca3c6f3aebc472d3520e44a4a376a0bcc75
theordore schrock <theorschrock@gmail.com>
2020-12-06 15:36:58 -0700

    added package_test_names.R

---
+++ R/package_test_names.R
@@ -0,0 +1,16 @@
+#' Get test names in a dev package.
+
+#' @name package_test_names
+#' @param path  \code{[directory]}  Defaults to \code{getwd()}
+#' @return \code{package_test_names}: [character]
+#' @export
+package_test_names <- function(path = getwd()) {
+    # Get test names in a dev package
+    assert_directory(path)
+    if (is_testthat_initialized(path)) {
+        g_stop("no test directory in dir '{path}'")
+    }
+    test_files <- list.files(path(path, "tests", "testthat")) %grep% "test_"
+    str_remove_all(test_files, "test_|\\.[rR]")
+    # Returns: [character]
+}
