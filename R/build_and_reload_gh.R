82c6ec203750658f56439fa51494dbac6bac68a3
theordore schrock <theorschrock@gmail.com>
2021-10-03 16:39:38 -0600

    added build_and_reload_gh.R

---
+++ R/build_and_reload_gh.R
@@ -0,0 +1,17 @@
+#' build a package from github.
+
+#' @name build_and_reload_gh
+#' @param package  \code{[package]}  Defaults to \code{current_pkg()}
+#' @param repo  \code{[string]}  Defaults to \code{'theorschrock1'}
+#' @param auth_token  \code{[string]}  All non-missing elements must comply to regex pattern \code{'''^ghp'''}.
+#' @return \code{build_and_reload_gh}: \code{[invisible(NULL)]}
+#' @export
+build_and_reload_gh <- function(package = current_pkg(), repo = "theorschrock1", 
+    auth_token) {
+    # build a package from github
+    assert_package(package)
+    assert_string(repo)
+    assert_string(auth_token, pattern = "^ghp")
+    remotes::install_github("{repo}/{package}", auth_token = auth_token)
+    # Returns: \code{[invisible(NULL)]}
+}
